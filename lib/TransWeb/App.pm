479a8283d0fda975abc211317f0861d0b0c0acf1
Jens Muecke <jens@nons.de>
2011-10-10 01:43:28 +0200

    route pages

--- lib/TransWeb/App.pm
+++ lib/TransWeb/App.pm
@@ -4,7 +4,16 @@ use Dancer ':syntax';
 our $VERSION = '0.1';
 
 get '/' => sub {
-    template 'index';
+    redirect '/site/start';;
 };
 
+get '/site/:page' => sub {
+        # check if valid page
+        return send_error("Seite nicht gefunden", 404) 
+            if (params->{page} =~ /^\d+$/);
+
+        # render page
+        template params->{page};
+};
+         
 true;
